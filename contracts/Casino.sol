134ea4a0c480862a3313684eec8ce63bc6b2c29f
Michael Victor <radmakson@gmail.com>
2025-08-21 12:49:02 +0100

    feat: refactor Casino contract to DecentralizedCasino with game state management

--- contracts/Casino.sol
+++ contracts/Casino.sol
@@ -1,14 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract Casino {
+contract DecentralizedCasino {
+    mapping(address => uint256) public gameWieValues;
+    mapping(address => uint256) public blockNumbersToBeUsed;
+
+    address public bank;
+
+    function fundBank() external payable {}
+
     function playGame() external payable {
+        uint256 blockNumberToBeUsed = blockNumbersToBeUsed[msg.sender];
+
+        if (blockNumberToBeUsed == 0) {
+            blockNumbersToBeUsed[msg.sender] = block.number + 2;
+            gameWieValues[msg.sender] = msg.value;
+            return;
+        }
+
+        require(block.number >= blockNumbersToBeUsed[msg.sender], "Too early");
+
         uint256 randomNumber = block.prevrandao;
 
         if (randomNumber % 2 == 0) {
-            uint256 winningAmount = msg.value * 2;
+            uint256 winningAmount = gameWieValues[msg.sender] * 2;
             (bool success, ) = msg.sender.call{value: winningAmount}("");
             require(success, "Transfer failed");
         }
+
+        blockNumbersToBeUsed[msg.sender] = 0;
+        gameWieValues[msg.sender] = 0;
     }
 }
