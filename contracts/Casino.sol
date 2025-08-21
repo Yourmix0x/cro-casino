2659d1b61860fb0d6ca3aada05f6465ba41cae75
Michael Victor <radmakson@gmail.com>
2025-08-21 14:42:12 +0100

    feat: enhance game mechanics with user and bank commitment functions

--- contracts/Casino.sol
+++ contracts/Casino.sol
@@ -7,7 +7,33 @@ contract DecentralizedCasino {
 
     address public bank;
 
+    bytes32 public userCommitment;
+    bytes32 public bankCommitment;
+
+    uint256 public userNumber;
+    uint256 public bankNumber;
+
     function fundBank() external payable {}
+
+    function setUserCommitment(bytes32 commitment) external payable {
+        require(userCommitment == 0x0);
+        userCommitment = commitment;
+    }
+
+    function revealUserNumber(uint256 number) external {
+        require(keccak256(abi.encodePacked(number)) == userCommitment);
+        userNumber = number;
+    }
+
+    function setBankCommitment(bytes32 commitment) external {
+        require(bankCommitment == 0x0);
+        bankCommitment = commitment;
+    }
+
+    function revealBankNumber(uint256 number) external {
+        require(keccak256(abi.encodePacked(number)) == bankCommitment);
+        bankNumber = number;
+    }
 
     function playGame() external payable {
         uint256 blockNumberToBeUsed = blockNumbersToBeUsed[msg.sender];
@@ -20,7 +46,7 @@ contract DecentralizedCasino {
 
         require(block.number >= blockNumbersToBeUsed[msg.sender], "Too early");
 
-        uint256 randomNumber = block.prevrandao;
+        uint256 randomNumber = bankNumber | userNumber; // XOR
 
         if (randomNumber % 2 == 0) {
             uint256 winningAmount = gameWieValues[msg.sender] * 2;
